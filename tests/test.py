0f724a26f67d27b912a15e6ec9b0ef237bb17d5b
Kumar Anirudha <mail@anirudha.dev>
2023-07-20 19:18:10 +0530

    initial commit

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from click.testing import CliRunner
+
+from shimpy.shimpy import shimpy
+
+def test_shimpy_cli():
+    runner = CliRunner()
+    result = runner.invoke(shimpy, ["Asia/Kolkata"])
+    assert result.exit_code == 0
+    assert "Hello, India!" in result.output
